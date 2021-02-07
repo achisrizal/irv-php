c814d04527674c24f8b843997f1ce595d3fca550
Achis Rizal <achisrizal@gmail.com>
2021-02-07 10:26:00 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,14 +6,6 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('auth/login');
-	}
-	public function register()
-	{
-		return view('auth/register');
-	}
-	public function user()
-	{
-		return view('user/index');
+		return view('welcome_message');
 	}
 }
